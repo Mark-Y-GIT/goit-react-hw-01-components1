0f27bc78c21aecd182ee182426d1016cffaabba4
Mark-Y <yakushyn.mark@gmail.com>
2021-10-26 02:36:21 +0300

    test commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import user from './data/user.json';
+import { Profile } from './components/Profile/Profile';
+function App() {
+  return (
+    <div className="App">
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
+}
+
+export default App;
